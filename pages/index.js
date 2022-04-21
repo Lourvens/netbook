e880b1570f36e962dc8a962b08868b3e4fac1369
lourvens <lourvens.luxamar@gmail.com>
2022-04-21 12:02:43 -0400

    created landing page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import Head from 'next/head'
-import About from '../components/About'
-import Header from '../components/Header'
-import styles from '../styles/Home.module.css'
+import About from 'components/About'
+import Achievement from 'components/Achievement'
+import Feature from 'components/Feature'
+import Header from 'components/Header'
+import Team from 'components/Team'
+import CTA from 'components/CTA'
+import Blog from 'components/blog'
+import Footer from 'components/footer'
+
+const styles = {
+  container: ""
+}
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
       <About />
+      <Achievement />
+      <Feature />
+      <Team />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
