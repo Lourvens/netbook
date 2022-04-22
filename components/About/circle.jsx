68701a39b8e725efe049960d6f81b9ec99e80657
lourvens <lourvens.luxamar@gmail.com>
2022-04-22 11:00:46 -0400

    SEO configuration

--- components/About/circle.jsx
+++ components/About/circle.jsx
@@ -5,6 +5,7 @@ const centerStyle = ""
 function Circle({ children }) {
   return (
     <div className="flex items-center ">
+      <link rel="shortcut icon" href="logo.svg" type="image/x-icon" />
       <div className="relative w-4 h-4 mr-4">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4 h-4 rounded-full bg-blue-100 mr-2"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2 h-2 rounded-full bg-blue-500 mr-2"></div>
--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,33 @@ import CTA from 'components/CTA'
 import Blog from 'components/blog'
 import Footer from 'components/footer'
 
-const styles = {
-  container: ""
-}
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
+      <Head>
+        <title>Netbook | your solutions for community</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <meta name="title" content="Netbook | Your solutions for community." />
+        <meta name="description" content="More than 2 billion people in over countries use socibook to stay in touch with friends & family."/>
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://netbook-2gw831yh9-lourvens.vercel.app/" />
+        <meta property="og:title" content="Netbook | Your solutions for community." />
+        <meta property="og:description" content="More than 2 billion people in over countries use socibook to stay in touch with friends & family."/>
+        <meta property="og:image" content="https://netbook-2gw831yh9-lourvens.vercel.app/presentation.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://netbook-2gw831yh9-lourvens.vercel.app/" />
+        <meta property="twitter:title" content="Netbook | Your solutions for community." />
+        <meta property="twitter:description" content="More than 2 billion people in over countries use socibook to stay in touch with friends & family." />
+        <meta property="twitter:image" content="https://netbook-2gw831yh9-lourvens.vercel.app/presentation.png"></meta>
+
+      </Head>
       <Header />
       <About />
       <Achievement />
